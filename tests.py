20dea646e197cb5027695d289f8a558c36ac7239
Steve Jarvis <steve.a.jarvis@gmail.com>
2013-05-31 18:13:06 -0400

    Fixed bug when URL was at end of tweet. Cleans better now.

--- tests.py
+++ tests.py
@@ -57,6 +57,13 @@ class SayTests(unittest.TestCase):
         s = 'this is a tweet http://twitpic.com/whtever #hash tweet'
         processed = speech.clean(s)
         self.assertEqual(processed, 'this is a tweet #hash tweet')
+        s = 'this is a tweet http://twitpic.com/whtever'
+        processed = speech.clean(s)
+        self.assertEqual(processed, 'this is a tweet ')
+        s = 'http://twitpic.com/whtever more #taga'
+        processed = speech.clean(s)
+        self.assertEqual(processed, 'more #taga')
+
 
 
 if __name__ == '__main__':
