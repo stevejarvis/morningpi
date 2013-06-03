eab2a84d3889e3804c930af6d67ce999b9ddfa6c
Steve Jarvis <steve.a.jarvis@gmail.com>
2013-06-03 16:25:08 -0400

    Changed to use the settings file

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from speech import say
 from tweets import get_tweets
+from weather import get_weather
+import settings
 
 def read_tweets(user, tweets):
     for tweet in tweets:
         say('%s tweeted %s' %(user, tweet))
+
+def read_weather():
+    # Currently, weather will be a dictionary with a comment, temp, and max.
+    location = settings.get_preference('location')
+    w = get_weather(location)
+    say('The weather for %s includes %s' %(location, w['comment']))
+    say('It is currently %d. Expect it to get %.2f degrees warmer'
+        %(w['temp'], w['max'] - w['temp']))
 
 def tweets(username, count=2):
     return get_tweets(username, count)[1]
 
 
 if __name__ == '__main__':
-    handles = ['JennyJarv', 'Chris_Lenahen', 'allieehenry', 'stevenjarvis']
+    handles = settings.get_preference('tweets')
     for user in handles:
-        t = tweets(user)
+        t = tweets(user, 1)
         read_tweets(user, t)
+    read_weather()
